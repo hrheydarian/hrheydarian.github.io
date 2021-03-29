40a62b32dd1144ad224694ab1a8f45fe0510183e
hamidreza <hr.heidarian@gmail.com>
2021-03-29 10:04:49 +0200

    edit

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Veldhoven, Netherlands",
+    51.40770635,
+    5.392731058176404
   ],
   [
-    "London, UK",
+    "College Station, TX, USA",
+    30.5955289,
+    -96.3071042
+  ],
+  [
+    "the Netherlands",
+    52.5001698,
+    5.7480821
+  ],
+  [
+    "Lausanne, Switzerland",
+    46.5218269,
+    6.6327025
+  ],
+  [
+    "London, United Kingdom",
     51.5073219,
-    -0.1276473
+    -0.1276474
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Berlin, Germany",
+    52.5170365,
+    13.3888599
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Delft, the Netherlands",
+    51.999457199999995,
+    4.362724538543995
   ]
 ];
